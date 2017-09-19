dbceb25014b42d9c866d7b3867a6cc6e532572b2
HaoHo <haho@codix.eu>
2017-09-19 14:37:00 +0700

    Save course successfully.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,9 @@
-import {createCourse, applyMiddleware} from "redux";
+import {createStore} from "redux";
 import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
-  return createCourse(
+  return createStore(
     rootReducer,
-    initialState,
-    applyMiddleware(reduxImmutableStateInvariant())
+    initialState
   );
 }
