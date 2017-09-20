e480763cdf6c3aa9af89b39a06e0b2dd8be4717a
HaoHo <haho@codix.eu>
2017-09-20 18:06:47 +0700

    CourseForm creation.

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,24 +2,37 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
+    this.state = {
+      course: Object.assign({}, this.props.course), errors: {}
+    };
   }
 
   render() {
     return (
-      <h1>Manage Course</h1>
+      <div>
+        <CourseForm
+          course={this.state.course}
+          errors={this.state.errors}
+          allAuthors={[]}
+        />
+      </div>
     );
   }
 }
 
-ManageCoursePage.propTypes = {};
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
+  let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
   return {
-    courses: state.courses
+    course: course
   };
 }
 
